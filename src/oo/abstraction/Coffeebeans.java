0ecb0614a5b07df8b0b7720f219edf09fc70a833
Sean <seanh737@gmail.com>
2017-03-12 21:14:24 +0800

    The progress of abstraction

---
+++ src/oo/abstraction/Coffeebeans.java
@@ -0,0 +1,9 @@
+package oo.abstraction;
+
+public class Coffeebeans {
+	String name;
+	String origin;
+	String Flavor;
+	int altitude;
+	String roast; 
+}
---
+++ src/oo/abstraction/CoffeebeansTester.java
@@ -0,0 +1,22 @@
+package oo.abstraction;
+
+public class CoffeebeansTester {
+
+	public static void main(String[] args) {
+		Coffeebeans CB1 = new Coffeebeans();
+		CB1.name = "Arabica";
+		CB1.origin = "East Timor";
+		CB1.altitude = 2000;
+		CB1.Flavor = "mellow";
+		CB1.roast = "Full City Roast";
+		
+		Coffeebeans CB2 = new Coffeebeans();
+		CB2.name = "Robusta";
+		CB2.origin = "Indonesia Java";
+		CB2.altitude = 200;
+		CB2.Flavor = "Bitter";
+		CB2.roast = "Full City Roast";
+		
+	}
+
+}
