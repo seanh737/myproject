a4cd2ce6ac45f8216cf643ee694a4166364727c6
Sean <seanh737@gmail.com>
2017-03-12 21:24:44 +0800

    The progress of abstraction

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -5,4 +5,11 @@ public class Car {
 	int cc;
 	String name;
 	String type;
+	
+	public Car(String brand, int cc, String name, String type){
+		this.brand = brand;
+		this.cc = cc;
+		this.name = name;
+		this.type = type;
+	}
 }
