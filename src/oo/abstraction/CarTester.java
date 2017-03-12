0fa8b6a02ff02ad0c5e6b881e055a78858fcd80b
Sean <seanh737@gmail.com>
2017-03-12 21:59:13 +0800

    The progress of abstraction

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -3,11 +3,11 @@ package oo.abstraction;
 public class CarTester {
 
 	public static void main(String[] args) {
-		Car c1 = new Car();
-		c1.brand = "Toyota";
-		c1.name = "Altis";
-		c1.cc = 1800;
-		c1.type = "Sedan";
+		Car c1 = new Car("Toyota", 1800, "Altis", "Sedan");
+//		c1.brand = "Toyota";
+//		c1.name = "Altis";
+//		c1.cc = 1800;
+//		c1.type = "Sedan";
 	}
 
 }
--- src/oo/abstraction/Coffeebeans.java
+++ src/oo/abstraction/Coffeebeans.java
@@ -6,4 +6,12 @@ public class Coffeebeans {
 	String Flavor;
 	int altitude;
 	String roast; 
+	
+	public Coffeebeans(String name, String origin, String Flavor, int altitude, String roast){
+		this.name = name;
+		this.origin = origin;
+		this.Flavor = Flavor;
+		this.altitude = altitude;
+		this.roast = roast;
+	}
 }
